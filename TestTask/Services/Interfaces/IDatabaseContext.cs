ca3e536c8f5498a7fbedb0673132ee388b711785
destdk <destdk@gmail.com>
2023-10-03 22:46:39 +0300

    Add project files.

---
+++ TestTask/Services/Interfaces/IDatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Models;
+
+namespace TestTask.Services.Interfaces
+{
+    public interface IDatabaseContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ TestTask/Services/UserService.cs
@@ -0,0 +1,28 @@
+﻿using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IDatabaseContext _dbContext;
+
+        public UserService(IDatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+
+
+        public Task<User> GetUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
